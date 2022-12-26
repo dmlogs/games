353879e55d76b5d2e130fb3288fe61e5ec1ad4b9
Dan Logan <dmlogs@gmail.com>
2022-12-26 23:26:44 +0000

    inital list

--- src/App.js
+++ src/App.js
@@ -1,27 +1,83 @@
 import './App.css';
 
 function App() {
+  let games = [
+    {
+      name: "Wingspan",
+      playersLow: 2,
+      playersHigh: 5
+    },
+    {
+      name: "Catan",
+      playersLow: 2,
+      playersHigh: 6
+    },
+    {
+      name: "Catan: Cities & Knights",
+      playersLow: 2,
+      playersHigh: 6
+    },
+    {
+      name: "Puerto Rico",
+      playersLow: 3,
+      playersHigh: 4
+    },
+    {
+      name: "Unstable Unicorns"
+    },
+    {
+      name: "Dominion"
+    },
+    {
+      name: "Art of Chill"
+    },
+    {
+      name: "Iota"
+    },
+    {
+      name: "Betrayal of House on the Hill"
+    },
+    {
+      name: "Splendor"
+    },
+    {
+      name: "Ticket to Ride"
+    },
+    {
+      name: "Ultimate Werewolf"
+    },
+    {
+      name: "Forbidden Island"
+    },
+    {
+      name: "Codenames"
+    },
+    {
+      name: "Cards Against Humanity"
+    },
+    {
+      name: "What Do You Meme?"
+    },
+    {
+      name: "It's In The Bag: Off The Wall",
+      playersLow: '4+'
+    }
+  ].sort((a,b) => {
+    return a.name?.localeCompare(b.name)
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <div style={{display: 'flex', alignItems: 'center', textAlign: 'center'}}>
+        <div style={{flex: 1}}><h2 style={{textDecoration: 'underline'}}>Name</h2></div>
+        <div style={{flex: 1}}><h2 style={{textDecoration: 'underline'}}>Players</h2></div>
+      </div>
+      {games.map(game => {
+        return (<div style={{display: 'flex', alignItems: 'center', textAlign: 'center'}}>
+          <div style={{flex: 1}}><h3>{game.name}</h3></div>
+          <div style={{flex: 1}}>{game.playersLow}{game.playersLow && game.playersHigh && " - "}{game.playersHigh}</div>
+        </div>)
+      })}
     </div>
   );
 }
